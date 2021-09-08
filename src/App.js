fa35bdbf57a1ad56b34371cd37f3725d44a61a73
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-09-07 22:05:24 -0700

    created class counter and hooks counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './Counter';
+import CounterHooks from './CounterHooks'
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  const [theme, setTheme] = useState('red')
+
   return (
-    <h1>Proof of Life</h1>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      <div>Counter</div>
+      <Counter initialCount={0}/>
+      <div>Counter Hooks</div>
+      <CounterHooks initialCount={0}/>
+      <button onClick={()  => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   );
 }
 
